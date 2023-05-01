22fdc476aad846637740d2490d551de1c3a74ad6
Sean P <sean@devusol.com>
2023-05-01 06:25:15 -0400

    sql qeuries and python

--- app.py
+++ app.py
@@ -8,16 +8,20 @@ app = Flask(__name__)
 
 app.config['MYSQL_DATABASE_USER'] = 'root'
 app.config['MYSQL_DATABASE_PASSWORD'] = ''
-app.config['MYSQL_DATABASE_DB'] = 'test'
+app.config['MYSQL_DATABASE_DB'] = 'users'
 app.config['MYSQL_DATABASE_HOST'] = 'localhost'
 mysql.init_app(app)
 
 conn = mysql.connect()
 cursor = conn.cursor()
 
+cursor.execute("CREATE TABLE IF NOT EXISTS bucketlist (user_id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255), email VARCHAR(255), password VARCHAR(255))")
+
+
 @app.route('/signup')
 def signup():
     return render_template("signup.html")
+
 
 @app.route('/api/signup', methods=['POST'])
 def signup_post():
@@ -26,20 +30,25 @@ def signup_post():
     _password = request.form['inputPassword']
 
     _hashed_password = generate_password_hash(_password)
-    cursor.callproc('sp_createUser',(_name,_email,_hashed_password))
-
+    # cursor.callproc('sp_createUser',(_name,_email,_hashed_password))
+    sql = "INSERT INTO bucketlist (name, email, password) VALUES (%s, %s, %s)"
+    val = (_name, _email, _hashed_password)
+    cursor.execute(sql, val)
+    conn.commit()
+    # print(cursor.rowcount, "record inserted.")
     data = cursor.fetchall()
     if len(data) == 0:
         conn.commit()
-        return json.dumps({'message':'User created successfully !'})
+        return json.dumps({'message': 'User created successfully !'})
     else:
-        return json.dumps({'error':str(data[0])})
+        return json.dumps({'error': str(data[0])})
 
 
 @app.route("/")
 def index():
     # return "Hello from index route"
     return render_template('index.html')
+
 
 if __name__ == "__main__":
     app.run()
