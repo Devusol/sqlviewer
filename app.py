2a3bc0565128cb9738b5c1b655f429312e9b0afe
devusol <sean@devusol.com>
2023-05-02 13:57:38 -0400

    files upload complete

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template, request, json
+import datetime
+from flask import Flask, render_template, request, json, redirect, url_for
 from flaskext.mysql import MySQL
 from werkzeug.security import generate_password_hash
 
@@ -6,10 +7,18 @@ mysql = MySQL()
 
 app = Flask(__name__)
 
-app.config['MYSQL_DATABASE_USER'] = 'stacktest_devusolhost'
-app.config['MYSQL_DATABASE_PASSWORD'] = 'Stackdb_-Pa55!'
-app.config['MYSQL_DATABASE_DB'] = 'users'
-app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+app.config['MYSQL_DATABASE_USER'] = 'devusol'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'Tulip_-Db'
+app.config['MYSQL_DATABASE_DB'] = 'tulip2023'
+app.config['MYSQL_DATABASE_HOST'] = '50.62.141.187'
+# app.config['MYSQL_USE_POOL'] = {
+#     # use = 0 no pool else use pool
+#     "use": 0,
+#     # size is >=0,  0 is dynamic pool
+#     "size": 10,
+#     # pool name
+#     "name": "local",
+# },
 mysql.init_app(app)
 
 conn = mysql.connect()
@@ -40,6 +49,52 @@ def signup_post():
     if len(data) == 0:
         conn.commit()
         return json.dumps({'message': 'User created successfully !'})
+    else:
+        return json.dumps({'error': str(data[0])})
+
+
+@app.route('/api/tulip2023', methods=['POST'])
+def tulip_post():
+    print("tulip route")
+    cursor = conn.cursor()
+
+    # _contest = request.form['submissiontype']
+    _firstname = request.form['fname']
+    _lastname = request.form['lname']
+    _email = request.form['email']
+    _phone = request.form['phone']
+    _add1 = request.form['add1']
+    _add2 = request.form['add2']
+    _city = request.form['city']
+    _state = request.form['state']
+    _zip = request.form['zip']
+    _files = request.files.getlist('uploads')
+    _filenames = ""
+
+    for file in _files:
+        x = datetime.datetime.now()
+        print(x.strftime("%m_%d_%H_%M"))
+        _filenames += (f", {file.filename}")
+        file.save(f"./uploads/{_firstname}_{_lastname}_{x.strftime('%m_%d_%H_%M')}_{file.filename}")
+        print(_filenames)
+
+    sql = "INSERT INTO ContestEntries (firstname, lastname, email, phone, address1, address2, city, state, zip,  files) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
+    val = (_firstname, _lastname, _email, _phone,
+           _add1, _add2, _city, _state, _zip,  _filenames)
+    cursor.execute(sql, val)
+    conn.commit()
+    # print(cursor.rowcount, "record inserted.")
+
+    data = cursor.fetchall()
+    print(data)
+    if len(data) == 0:
+        try:
+            conn.commit()
+            json.dumps({'message': 'User created successfully !'})
+            cursor.close()
+            return redirect('http://localhost:3000/tulip-contest.php')
+        except Exception as e:
+            print('Failed to commit to db: ' + e)
     else:
         return json.dumps({'error': str(data[0])})
 
